b9d7a0443dfc550d97dcd8cdc4c933887af1e32d
Simen Bekkhus <sbekkhus91@gmail.com>
2013-10-16 22:57:26 +0200

    PG3200: Made the buttons to start exercises into a listview

--- pg3200/exercises/src/main/java/no/nith/pg3200/exercises/Splash.java
+++ pg3200/exercises/src/main/java/no/nith/pg3200/exercises/Splash.java
@@ -1,15 +1,19 @@
 package no.nith.pg3200.exercises;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import no.nith.pg3200.exercises.exercise2.Exercise2;
 import no.nith.pg3200.exercises.exercise3.Exercise3;
 import no.nith.pg3200.exercises.exercise4.Exercise4;
 
-public class Splash extends Activity implements View.OnClickListener {
+public class Splash extends Activity {
     /**
      * Called when the activity is first created.
      */
@@ -18,27 +22,33 @@ public class Splash extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        final Button task2 = (Button) findViewById(R.id.exercise2);
-        final Button task3 = (Button) findViewById(R.id.exercise3);
-        final Button task4 = (Button) findViewById(R.id.exercise4);
+        final Context context = this;
 
-        task2.setOnClickListener(this);
-        task3.setOnClickListener(this);
-        task4.setOnClickListener(this);
-    }
+        final ListView listView = (ListView) findViewById(R.id.splashListView);
 
-    @Override
-    public void onClick(final View v) {
-        switch (v.getId()) {
-        case R.id.exercise2:
-            startActivity(new Intent(this, Exercise2.class));
-            break;
-        case R.id.exercise3:
-            startActivity(new Intent(this, Exercise3.class));
-            break;
-        case R.id.exercise4:
-            startActivity(new Intent(this, Exercise4.class));
-            break;
-        }
+        final String[] listOfExercises = new String[]{
+                "Exercise2", "Exercise3", "Exercise4"
+        };
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listOfExercises);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (((TextView) view).getText().toString()) {
+                case "Exercise2":
+                    startActivity(new Intent(context, Exercise2.class));
+                    break;
+                case "Exercise3":
+                    startActivity(new Intent(context, Exercise3.class));
+                    break;
+                case "Exercise4":
+                    startActivity(new Intent(context, Exercise4.class));
+                    break;
+                }
+            }
+        });
     }
 }
